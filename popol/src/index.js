ce923c4e2e60f07077a232e50d38d9e49c0002a1
dlfdydwlrshehdwk <118067728+dlfdydwlrshehdwk@users.noreply.github.com>
2023-07-03 15:26:43 +0900

    41

--- popol/src/index.js
+++ popol/src/index.js
@@ -13,7 +13,23 @@ export default function App(){
   useEffect(()=>{
     $(window).on('wheel',function(e){
       let a = e.originalEvent.wheelDelta
-      console.log(a)
+      let i = 0;
+      // console.log(a)
+
+      if(a < 0){
+        console.log('밑으로')
+        i++
+        // 만약 ul을 100으로 나눈숫자보다 커진다면 그숫자+1 보다 작게
+      }
+      else{
+        console.log('위로')
+        i--
+        // 만약 i가 0보다 작아진다면 i는 0
+      }
+
+      // 트랜스폼을 i * 100px로 셋팅 
+
+      // 부드럽게는 어떻게? .1초마다 ? <- ? 
     })
   },[])
   return(
